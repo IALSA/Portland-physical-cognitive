2d85bddd23dfa44ebf1ec742626fd46ea4373166
Andrey Koval <koval.andrey@gmail.com>
2015-12-21 08:45:59 -0800

    post processing script

---
+++ manipulation/rename-classify.R
@@ -0,0 +1,20 @@
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+
+# ---- load_sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+
+# ---- load_packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) #Pipes
+
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("ggplot2", quietly=TRUE)
+requireNamespace("dplyr", quietly=TRUE) #Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("testit", quietly=TRUE)
+# requireNamespace("plyr", quietly=TRUE)
+
+# ---- declare_globals ---------------------------------------------------------
+path_input  <- "./data-phi-free/raw/mtcar.csv"
+path_output <- "./data-phi-free/derived/motor-trend-car-test.rds"
+figure_path <- 'manipulation/stitched-output/te/'
